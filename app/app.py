a90e5d4472d7502661d4be2f4633f050eba8b585
Elaine Liu <eliu72@uwo.ca>
2021-04-11 20:35:46 -0400

    working api

--- app/app.py
+++ app/app.py
@@ -21,23 +21,23 @@ app.config["DEBUG"] = True
 # tutorial
 # https://programminghistorian.org/en/lessons/creating-apis-with-python-and-flask
 
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
+# books = [
+#     {'id': 0,
+#      'title': 'A Fire Upon the Deep',
+#      'author': 'Vernor Vinge',
+#      'first_sentence': 'The coldsleep itself was dreamless.',
+#      'year_published': '1992'},
+#     {'id': 1,
+#      'title': 'The Ones Who Walk Away From Omelas',
+#      'author': 'Ursula K. Le Guin',
+#      'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
+#      'published': '1973'},
+#     {'id': 2,
+#      'title': 'Dhalgren',
+#      'author': 'Samuel R. Delany',
+#      'first_sentence': 'to wound the autumnal city.',
+#      'published': '1975'}
+# ]
 
 legs = [
     {
